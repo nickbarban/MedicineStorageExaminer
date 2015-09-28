f610d43fe79b0e3f9eca1782343307004ce1702c
Nick Barban <nn.barban@gmail.com>
2015-09-28 18:36:27 +0300

    Add analyzer

---
+++ data-crawler/src/main/java/service/PDFExtractorException.java
@@ -0,0 +1,11 @@
+package service;
+
+public class PDFExtractorException extends Exception {
+
+	public PDFExtractorException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	
+}
