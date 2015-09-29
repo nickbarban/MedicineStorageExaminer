e3f6d232092ce9376711f232dce75f1452c8e792
Nick Barban <nn.barban@gmail.com>
2015-09-30 02:52:42 +0300

    add Leftover class and add creating leftovers at PdfExtractorTestClient and update Medical class for List of leftovers

---
+++ data-crawler/src/main/java/domain/Leftover.java
@@ -0,0 +1,41 @@
+package domain;
+
+public class Leftover {
+
+	private int localId;
+	private String name;
+	private String measure;
+	private double quantity;
+	
+	public Leftover(int localId, String name, String measure, double quantity) {
+		this.localId = localId;
+		this.name = name;
+		this.quantity = quantity;
+		this.measure = measure;
+	}
+
+	public int getLocalId() {
+		return localId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public double getQuantity() {
+		return quantity;
+	}
+
+	public String getMeasure() {
+		return measure;
+	}
+
+	@Override
+	public String toString() {
+		return "Leftover [" + localId + " " + name + " " + measure + " " + quantity
+				+ "]";
+	}
+	
+	
+	
+}
--- data-crawler/src/main/java/service/PdfExtractorTestClient.java
+++ data-crawler/src/main/java/service/PdfExtractorTestClient.java
@@ -4,15 +4,95 @@ package service;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStream;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+
+import org.jsoup.nodes.Document;
+
+import domain.Leftover;
+import domain.Medical;
 
 public class PdfExtractorTestClient {	
 	
     public static void main(String[] args) {
-    	List<String> unParsableFiles = new ArrayList<>();
-    	final String[] fileNames = {"ananiev_cpsmd.pdf", "ananiev_crl.pdf", "arciz_cpsmd.pdf", "arciz_crl.pdf",
+    	// delete this line when SiteParserServiceBean.parse() will execute correctly
+    	ClassLoader classLoader = PdfExtractorTestClient.class.getClassLoader(); 
+    	List<Medical> medicals = new ArrayList<>();
+    	List<URI> unParsableLinks = new ArrayList<>();
+    	try {
+			Document site = SiteDownloader.download();
+			//System.out.println(site.baseUri());
+			SiteParserServiceBean siteParserService = new SiteParserServiceBean(site);
+			Map<Medical, String> links = siteParserService.parse();
+			//for (Medical medical : links.keySet()) { // uncomment this
+				//URI link = new URI(links.get(medical)); // uncomment this
+				InputStream inputStream = new FileInputStream((classLoader.getResource("example.pdf").getFile()));
+				String textFromPDF = PdfExtractor.extract(inputStream);
+				DataAnalyzer analyzer = new DataAnalyzer();
+				analyzer.analyze(textFromPDF);
+				List<String> analyzedData = analyzer.getAnalyzedData();
+				List<String> unAnalyzedData = analyzer.getUnAnalyzedData();
+				if (analyzedData.size() > 0) {
+					for (String string : analyzedData) {
+						int localId = 0;
+						double quantity = -1;
+						String measure = null;
+						String[] splitted = string.split(" ");
+												
+						if (splitted[0].matches("(\\d+)(\\W*)")) {
+							localId = Integer.parseInt(splitted[0].trim().split("\\W")[0]);
+							//System.out.println("localID: " + localID);
+						} //else System.out.println("WRONG localID: " + splitted[0]);
+						
+						if (splitted[splitted.length - 1].matches("(\\d+)")) {
+							
+							quantity = Double.parseDouble(splitted[splitted.length - 1].trim());
+						} else {
+							if (splitted[splitted.length - 1].matches("(\\d+)(\\W)(\\d+)")) {
+								String[] splittedQuantity = splitted[splitted.length - 1].trim().split(",");
+								String dottedQuantity = splittedQuantity[0] + "." + splittedQuantity[1];
+								quantity = Double.parseDouble(dottedQuantity.trim());
+							}
+						}
+						
+						StringBuilder name = new StringBuilder();
+						for (int i = 1; i < splitted.length-2; i++) {
+							if (!splitted[i].isEmpty() && !splitted[i].matches("(\\s+)")) {
+								name.append(splitted[i].trim() + " ");
+							}
+						}
+						
+						measure = splitted[splitted.length - 2].trim();
+						
+						Leftover leftover = new Leftover(localId, name.toString().trim(), measure, quantity);
+						System.out.println(leftover);
+						//medical.getLeftovers().add(leftover);
+					}
+				} else {
+					//unParsableLinks.add(link);  // uncomment this
+				}
+				// medicals.add(medical); // uncomment this
+			//} // uncomment this
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} /*catch (URISyntaxException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}*/ catch (PDFExtractorException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	
+    	
+    	
+    	/*List<String> unParsableFiles = new ArrayList<>();
+    	final String[] fileNames = {"example.pdf""ananiev_cpsmd.pdf", "ananiev_crl.pdf", "arciz_cpsmd.pdf", "arciz_crl.pdf",
     								"balta_cpsmd.pdf", "balta_crl.pdf", "bdnistrov_bd.pdf", "bdnistrov_crl.pdf",
     								"bdnistrov_kvd.pdf", "bdnistrov_mbl.pdf", "bdnistrov_mu_25.08.15.pdf", 
     								"bdnistrov_skrl.pdf", "bdnistrov_td2.pdf", "berezivka_cpsmd.pdf",
@@ -62,13 +142,13 @@ public class PdfExtractorTestClient {
 			}
 		}
     	try {
-			inputStream = new FileInputStream(new File(classLoader.getResource(unParsableFiles.get(1)).getFile()));
+			inputStream = new FileInputStream(new File(classLoader.getResource(unParsableFiles.get(0)).getFile()));
 			String textFromPDF = PdfExtractor.extract(inputStream);
-			System.out.println(unParsableFiles.get(1));
+			System.out.println(unParsableFiles.get(0));
 			System.out.println(textFromPDF);
 		} catch (FileNotFoundException | PDFExtractorException e) {
 			e.printStackTrace();
-		}
+		}*/
 		
     }
 }
